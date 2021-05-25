a47e6d0599625685d7cde5acc219dbb809e05621
Akib <akib3366@gmail.com>
2021-05-25 08:34:03 +0600

    sound board done

---
+++ 9-sound-board/script.js
@@ -0,0 +1,33 @@
+const sounds = ['applause', 'boo', 'gasp', 'tada', 'victory', 'wrong'];
+
+sounds.forEach((sound) => {
+    const btn = document.createElement('button');
+    btn.classList.add('btn');
+
+
+    btn.innerText = sound;
+
+    btn.addEventListener('click', () => {
+        stopSound();
+        document.getElementById(sound).play();
+
+    })
+
+
+    document.getElementById('buttons').appendChild(btn);
+
+})
+
+function stopSound() {
+    sounds.forEach(
+        (sound) => {
+            song = document.getElementById(sound);
+
+            song.pause();
+            song.currentTime = 0;
+
+
+        }
+    )
+
+}
